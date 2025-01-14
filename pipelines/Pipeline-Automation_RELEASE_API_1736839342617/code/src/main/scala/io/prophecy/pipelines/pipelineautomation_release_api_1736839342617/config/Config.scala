897b351b663baca07b731a38dcb43facf555368e
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-01-14 07:31:16 +0000

    Add new pipeline, dataset, job, and gem for Project-Automation Scala release API

---
+++ pipelines/Pipeline-Automation_RELEASE_API_1736839342617/code/src/main/scala/io/prophecy/pipelines/pipelineautomation_release_api_1736839342617/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomation_release_api_1736839342617.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ pipelines/Pipeline-Automation_RELEASE_API_1736839342617/code/src/main/scala/io/prophecy/pipelines/pipelineautomation_release_api_1736839342617/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomation_release_api_1736839342617.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)

c920f37945072b1c4906f49b383305e910ad43c2
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-01-23 13:53:38 +0000

    Add new pipeline, dataset, job, and gem for Project-Automation Scala release automation.

---
+++ pipelines/Pipeline-Automation_RELEASE_API_1737639867999/code/src/main/scala/io/prophecy/pipelines/pipelineautomation_release_api_1737639867999/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomation_release_api_1737639867999.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ pipelines/Pipeline-Automation_RELEASE_API_1737639867999/code/src/main/scala/io/prophecy/pipelines/pipelineautomation_release_api_1737639867999/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomation_release_api_1737639867999.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }

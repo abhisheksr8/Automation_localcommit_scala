e5261909a4f0254527cf58b4b9094ffc505c786c
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-05-21 16:22:45 +0000

    Add Project-Automation components including new pipeline, dataset, job, and gem

---
+++ pipelines/Pipeline-Automation_RELEASE_API_1747843943535/code/src/main/scala/io/prophecy/pipelines/pipelineautomation_release_api_1747843943535/main.scala
@@ -0,0 +1,48 @@
+package io.prophecy.pipelines.pipelineautomation_release_api_1747843943535
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.pipelineautomation_release_api_1747843943535.config._
+import io.prophecy.pipelines.pipelineautomation_release_api_1747843943535.functions.UDFs._
+import io.prophecy.pipelines.pipelineautomation_release_api_1747843943535.functions.PipelineInitCode._
+import io.prophecy.pipelines.pipelineautomation_release_api_1747843943535.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+
+  def apply(context: Context): Unit = {
+    val df_Project_Automation_scala_RELEASE_API_1747843943535_dataSet =
+      Project_Automation_scala_RELEASE_API_1747843943535_dataSet(context)
+    val df_customer_details_selection = customer_details_selection(
+      context,
+      df_Project_Automation_scala_RELEASE_API_1747843943535_dataSet
+    )
+  }
+
+  def main(args: Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Pipeline-Automation_RELEASE_API_1747843943535")
+      .enableHiveSupport()
+      .getOrCreate()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Pipeline-Automation_RELEASE_API_1747843943535"
+    )
+    spark.conf.set("spark.default.parallelism",             "4")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    registerUDFs(spark)
+    MetricsCollector.instrument(
+      spark,
+      "pipelines/Pipeline-Automation_RELEASE_API_1747843943535"
+    ) {
+      apply(context)
+    }
+  }
+
+}
